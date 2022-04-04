bcf5237405d72540616e91dcf57f916c0cca92bb
D-LAND\Leon <d-landarrese80@hotmail.com>
2022-04-03 23:22:16 -0500

    nuevos cambios de projects

--- src/components/Pages/Projects/Projects.jsx
+++ src/components/Pages/Projects/Projects.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
-import img1 from '../../../static/img/page-4_img01_original.jpg';
+import img1 from '../../../static/img/page-4_img01.jpg';
+import img2 from '../../../static/img/page-4_img02.jpg';
+import img3 from '../../../static/img/page-4_img03.jpg';
+import img4 from '../../../static/img/page-4_img04.jpg';
+import img5 from '../../../static/img/page-4_img05.jpg';
+import img6 from '../../../static/img/page-4_img06.jpg';
+import img7 from '../../../static/img/page-4_img07.jpg';
+import img8 from '../../../static/img/page-4_img08.jpg';
+import img9 from '../../../static/img/page-4_img09.jpg';
 import {
 	Card,
 	CardContent,
 	CardImage,
 	Cards,
+	CardThumb,
+	Container,
+	Link,
+	Paragraph,
+	Parallax,
+	ParallaxContent,
+	ParallaxImage,
+	ParallaxParagraph,
+	ParallaxTitle,
+	ParallaxWrapper,
 	Section,
+	Subtitle,
 	Title,
 	Wrapper,
 } from './style';
@@ -14,24 +33,164 @@ const Projects = () => {
 	return (
 		<Wrapper>
 			<Section>
-				<Title>
-					our products <span></span>
-				</Title>
-				<Cards>
-					<Card>
-						<CardImage src={img1}></CardImage>
-						<CardContent>
-							<h6>
-								<a href="#">Quisque a quam dictum, rutrum</a>{' '}
-							</h6>
-							<p>
-								Lorem ipsum dolor sit amet, coetuer adipiscing elit. Praesent
-								vestibulum molestie nonummy varius.
-							</p>
-						</CardContent>
-					</Card>
-				</Cards>
+				<Container>
+					<Title>
+						our products <span></span>
+					</Title>
+					<Cards>
+						<Card>
+							<CardThumb>
+								<CardImage src={img1}></CardImage>
+							</CardThumb>
+							<CardContent>
+								<Subtitle>
+									<Link href="/">Quisque a quam dictum, rutrum</Link>
+								</Subtitle>
+								<Paragraph>
+									Lorem ipsum dolor sit amet, coetuer adipiscing elit. Praesent
+									vestibulum molestie nonummy varius.
+								</Paragraph>
+							</CardContent>
+						</Card>
+						<Card>
+							<CardThumb>
+								<CardImage src={img2}></CardImage>
+							</CardThumb>
+							<CardContent>
+								<Subtitle>
+									<Link href="/">Quisque a quam dictum, rutrum</Link>
+								</Subtitle>
+								<Paragraph>
+									Lorem ipsum dolor sit amet, coetuer adipiscing elit. Praesent
+									vestibulum molestie nonummy varius.
+								</Paragraph>
+							</CardContent>
+						</Card>
+						<Card>
+							<CardThumb>
+								<CardImage src={img3}></CardImage>
+							</CardThumb>
+							<CardContent>
+								<Subtitle>
+									<Link href="/">Quisque a quam dictum, rutrum</Link>
+								</Subtitle>
+								<Paragraph>
+									Lorem ipsum dolor sit amet, coetuer adipiscing elit. Praesent
+									vestibulum molestie nonummy varius.
+								</Paragraph>
+							</CardContent>
+						</Card>
+					</Cards>
+					<Cards>
+						<Card>
+							<CardThumb>
+								<CardImage src={img4}></CardImage>
+							</CardThumb>
+							<CardContent>
+								<Subtitle>
+									<Link href="/">Quisque a quam dictum, rutrum</Link>
+								</Subtitle>
+								<Paragraph>
+									Lorem ipsum dolor sit amet, coetuer adipiscing elit. Praesent
+									vestibulum molestie nonummy varius.
+								</Paragraph>
+							</CardContent>
+						</Card>
+						<Card>
+							<CardThumb>
+								<CardImage src={img5}></CardImage>
+							</CardThumb>
+							<CardContent>
+								<Subtitle>
+									<Link href="/">Quisque a quam dictum, rutrum</Link>
+								</Subtitle>
+								<Paragraph>
+									Lorem ipsum dolor sit amet, coetuer adipiscing elit. Praesent
+									vestibulum molestie nonummy varius.
+								</Paragraph>
+							</CardContent>
+						</Card>
+						<Card>
+							<CardThumb>
+								<CardImage src={img6}></CardImage>
+							</CardThumb>
+							<CardContent>
+								<Subtitle>
+									<Link href="/">Quisque a quam dictum, rutrum</Link>
+								</Subtitle>
+								<Paragraph>
+									Lorem ipsum dolor sit amet, coetuer adipiscing elit. Praesent
+									vestibulum molestie nonummy varius.
+								</Paragraph>
+							</CardContent>
+						</Card>
+					</Cards>
+					<Cards>
+						<Card>
+							<CardThumb>
+								<CardImage src={img7}></CardImage>
+							</CardThumb>
+							<CardContent>
+								<Subtitle>
+									<Link href="/">Quisque a quam dictum, rutrum</Link>
+								</Subtitle>
+								<Paragraph>
+									Lorem ipsum dolor sit amet, coetuer adipiscing elit. Praesent
+									vestibulum molestie nonummy varius.
+								</Paragraph>
+							</CardContent>
+						</Card>
+						<Card>
+							<CardThumb>
+								<CardImage src={img8}></CardImage>
+							</CardThumb>
+							<CardContent>
+								<Subtitle>
+									<Link href="/">Quisque a quam dictum, rutrum</Link>
+								</Subtitle>
+								<Paragraph>
+									Lorem ipsum dolor sit amet, coetuer adipiscing elit. Praesent
+									vestibulum molestie nonummy varius.
+								</Paragraph>
+							</CardContent>
+						</Card>
+						<Card>
+							<CardThumb>
+								<CardImage src={img9}></CardImage>
+							</CardThumb>
+							<CardContent>
+								<Subtitle>
+									<Link href="/">Quisque a quam dictum, rutrum</Link>
+								</Subtitle>
+								<Paragraph>
+									Lorem ipsum dolor sit amet, coetuer adipiscing elit. Praesent
+									vestibulum molestie nonummy varius.
+								</Paragraph>
+							</CardContent>
+						</Card>
+					</Cards>
+				</Container>
 			</Section>
+			<Parallax>
+				<ParallaxImage></ParallaxImage>
+				<ParallaxWrapper>
+					<ParallaxContent>
+						<Container>
+							<ParallaxTitle>
+								we work hard <br /> for your satisfaction!
+							</ParallaxTitle>
+							<ParallaxParagraph>
+								Suspendisse potenti. Nulla pretium orci vel massa convallis
+								fermentum. Maecenas in convallis magna. Donec sem enim, congue
+								non risus non, tincidunt viverra mauris. Mauris at dolor tempor,
+								finibus mi sed, feugiat ex. Ut rhoncus velit eget lectus tempor
+								interdum. Sed nec mi eu arcu ultrices scelerisque in dignissim
+								enim.
+							</ParallaxParagraph>
+						</Container>
+					</ParallaxContent>
+				</ParallaxWrapper>
+			</Parallax>
 		</Wrapper>
 	);
 };
--- src/components/Pages/Projects/style.js
+++ src/components/Pages/Projects/style.js
@@ -1,38 +1,68 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.main`
-	background-color: rgb(26, 27, 26);
-	height: 100vh;
+	height: auto;
+	margin-right: 0;
+	margin-left: 0;
 `;
 
 export const Section = styled.section`
+	background-color: rgb(26, 27, 26);
 	padding-bottom: 103px;
 	padding-top: 91px;
 `;
 
+export const Container = styled.div`
+	width: 1170px;
+	margin-right: auto;
+	margin-left: auto;
+	@media (max-width: 992px) {
+		width: 970px;
+	}
+	@media (max-width: 768px) {
+		width: 750px;
+		padding-left: 0;
+		padding-right: 0;
+	}
+`;
+
 export const Title = styled.h2`
-	color: rgb(215, 208, 195);
 	font-size: 40px;
 	line-height: 48px;
 	text-transform: uppercase;
 	color: rgb(215, 208, 195);
 	overflow: hidden;
+	@media (max-width: 768px) {
+		margin-left: 3rem;
+	}
 `;
 
 export const Cards = styled.div`
 	display: grid;
-	grid-template-columns: repeat(3, 1fr);
-	grid-template-rows: repeat(3, 1fr);
-	gap: 20px;
-	margin-top: 40px;
+	grid-template-columns: repeat(auto-fill, minmax(250px, 3fr));
+	gap: 40px;
+	width: 95%;
+	margin-bottom: 40px;
+	@media (max-width: 992px) {
+		width: 82%;
+	}
+	@media (max-width: 768px) {
+		width: 45%;
+		margin-left: 3rem;
+	}
 `;
 
 export const Card = styled.div`
 	box-shadow: 0 0 3px rgba(0, 0, 0, 0.1);
 `;
 
+export const CardThumb = styled.a`
+	display: block;
+`;
+
 export const CardImage = styled.img`
 	width: 100%;
+	height: auto;
 `;
 
 export const CardContent = styled.div`
@@ -41,4 +71,75 @@ export const CardContent = styled.div`
 	line-height: 1.5rem;
 	font-size: 0.9rem;
 	padding: 25px 20px 35px;
+`;
+
+export const Subtitle = styled.h6`
+	color: rgb(215, 208, 195);
+	font-size: 20px;
+	line-height: 14px;
+	margin-bottom: 0;
+	margin-top: 0;
+`;
+
+export const Link = styled.a`
+	text-decoration: none;
+	color: rgb(215, 208, 195);
+	cursor: pointer;
+
+	&:hover {
+		text-decoration-color: currentcolor;
+		color: rgb(114, 165, 193);
+	}
+`;
+
+export const Paragraph = styled.p`
+	font-size: 15px;
+	line-height: 20px;
+`;
+
+export const Parallax = styled.section`
+	position: relative;
+	overflow: hidden;
+`;
+
+export const ParallaxImage = styled.div`
+	background-image: url('src/static/img/parallax3.jpg');
+	background-color: inherit;
+	height: 500px;
+	transform: translate3d(0px, -132.533px, 0px);
+	background-repeat: no-repeat;
+	background-size: cover;
+	position: absolute;
+	top: 0;
+	left: 0;
+	right: 0;
+	background-position: center center;
+	will-change: transform;
+`;
+
+export const ParallaxWrapper = styled.div`
+	position: relative;
+	padding-bottom: 144px;
+	padding-top: 120px;
+`;
+
+export const ParallaxContent = styled.div`
+	position: relative;
+
+	color: rgb(244, 237, 226);
+`;
+
+export const ParallaxTitle = styled.span`
+	font-size: 80px;
+	font-weight: 700;
+	line-height: 70px;
+	text-transform: uppercase;
+	@media (max-width: 992px) {
+		font-size: 30px;
+		line-height: 35px;
+	}
+`;
+
+export const ParallaxParagraph = styled.p`
+	font-size: 19px;
 `;
