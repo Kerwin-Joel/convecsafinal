9fea198ad85a8b6aedd6fcff36acf0da5403f60b
XP65382 <XP65382>
2022-04-08 16:28:32 -0500

    add router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,19 @@ import { Nav } from './nav/Nav';
 import { Footer } from './footer/Footer';
 import Projects from './Pages/Projects/Projects';
 import { Home } from './Pages/home/Home';
+import { Routes, Route, Link } from "react-router-dom";
+
+
 
 export const App = () => {
 	return (
 		<>
-			<Nav />
-			{/* <Home/> */}
-			<Projects />
-			<Footer />
+    <Nav />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="about" element={<Projects />} />
+    </Routes>
+    <Footer />
 		</>
 	);
 };
