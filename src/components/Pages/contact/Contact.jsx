e8fe9de5c076468125c09b2102deb165ab57f815
D-LAND\Leon <d-landarrese80@hotmail.com>
2022-04-17 16:06:16 -0500

    funcionamiento del input

--- src/components/Pages/contact/Contact.jsx
+++ src/components/Pages/contact/Contact.jsx
@@ -6,10 +6,53 @@ import Loader from '../../utils/Loader';
 import { Toaster, toast } from 'react-hot-toast';
 
 export const Contact = () => {
-	const [error, setError] = useState(null);
 	const [loading, setLoading] = useState(false);
 	const form = useRef();
+	const [toggle, setToggle] = useState(false);
+	const divRef = useRef(null);
+	const divRef1 = useRef(null);
+	const divRef2 = useRef(null);
+	const divRef3 = useRef(null);
+	const handleInput1 = e => {
+		console.log(e.target.value);
 
+		if (e.target.value.length > 0) {
+			setToggle(!toggle);
+			divRef.current.style.display = 'none';
+		} else {
+			divRef.current.style.display = 'block';
+		}
+	};
+
+	const handleInput2 = e => {
+		if (e.target.value.length > 0) {
+			setToggle(!toggle);
+
+			divRef1.current.style.display = 'none';
+		} else {
+			divRef1.current.style.display = 'block';
+		}
+	};
+
+	const handleInput3 = e => {
+		if (e.target.value.length > 0) {
+			setToggle(!toggle);
+
+			divRef2.current.style.display = 'none';
+		} else {
+			divRef2.current.style.display = 'block';
+		}
+	};
+
+	const handleInput4 = e => {
+		if (e.target.value.length > 0) {
+			setToggle(!toggle);
+
+			divRef3.current.style.display = 'none';
+		} else {
+			divRef3.current.style.display = 'block';
+		}
+	};
 	const sendEmail = e => {
 		e.preventDefault();
 		setLoading(true);
@@ -62,21 +105,21 @@ export const Contact = () => {
 						<form ref={form} onSubmit={sendEmail}>
 							<section>
 								<div>
-									<input type="text" name="from_name" />
-									<label>Nombre</label>
+									<input type="text" name="from_name" onChange={handleInput1} />
+									<label ref={divRef}>Nombre</label>
 								</div>
 								<div>
-									<input type="email" name="email" />
-									<label>Email</label>
+									<input type="email" name="email" onChange={handleInput2} />
+									<label ref={divRef1}>Email</label>
 								</div>
 								<div>
-									<input type="phone" name="phone" />
-									<label>Teléfono</label>
+									<input type="phone" name="phone" onChange={handleInput3} />
+									<label ref={divRef2}>Teléfono</label>
 								</div>
 							</section>
 							<main>
-								<textarea name="message"></textarea>
-								<label>Mensaje</label>
+								<textarea name="message" onChange={handleInput4}></textarea>
+								<label ref={divRef3}>Mensaje</label>
 							</main>
 							<button> {loading ? <Loader /> : 'Enviar'}</button>
 						</form>
--- src/components/Pages/contact/styles.js
+++ src/components/Pages/contact/styles.js
@@ -4,53 +4,51 @@ export const Container = styled.div`
 	padding: 5% 10%;
 	padding-bottom: 8%;
 
-  & section{
-    display: flex;
-    align-items: center;
-    gap: 5%;
-  }
+	& section {
+		display: flex;
+		align-items: center;
+		gap: 5%;
+	}
 
-  & span{
-    display: block;
-    width: 100%;
-    height: 1px;
-    background: #479ed487;
-  }
+	& span {
+		display: block;
+		width: 100%;
+		height: 1px;
+		background: #479ed487;
+	}
 
-  & h1{
-    font-size: 40px;
-  }
-
+	& h1 {
+		font-size: 40px;
+	}
 `;
 
 export const Content = styled.div`
 	display: grid;
 	grid-template-columns: 30% 70%;
-  
-  h2,h1{
-    color:#25292e;
-  }
 
-  @media (max-width: 980px) {
+	h2,
+	h1 {
+		color: #25292e;
+	}
+
+	@media (max-width: 980px) {
 		grid-template-columns: repeat(auto-fit, minmax(15rem, 100%));
 	}
 `;
 export const Left = styled.div`
-
 	h2 {
 		font-size: 1.5em;
-		
 	}
-	p,li {
+	p,
+	li {
 		font-size: 0.85em;
 		margin-top: 0;
 	}
-  ul > li{ 
-    padding: 1.5% 0;
-  }
+	ul > li {
+		padding: 1.5% 0;
+	}
 `;
 export const Rigth = styled.div`
-
 	section {
 		display: grid;
 		grid-template-columns: repeat(auto-fit, minmax(10rem, 1fr));
@@ -71,8 +69,7 @@ export const Rigth = styled.div`
 			color: #626262;
 			border: 1px solid #cccccc;
 			border-radius: 0;
-      border-radius:5px;
-
+			border-radius: 5px;
 		}
 		& > div {
 			position: relative;
@@ -80,76 +77,76 @@ export const Rigth = styled.div`
 		}
 
 		label {
-      position: absolute;
-      top: 25%;
-      left: 5%;
-      color: #a9a9a9;
-      transition: all 0.3s ease-in-out;
+			position: absolute;
+			top: 25%;
+			left: 5%;
+			color: #a9a9a9;
+			transition: all 0.3s ease-in-out;
 		}
-    
 	}
-  input:focus + label{
-    top: 7%;
-    left: 3%;
-    font-size: 13px;
-    color: #a9a9a9;
-  }
+	input:focus + label {
+		top: 7%;
+		left: 3%;
+		font-size: 13px;
+		color: #a9a9a9;
+	}
 
 	main {
 		position: relative;
 		margin-top: 2rem;
 
 		label {
-      position: absolute;
-      top: 5%;
-      left: 1.5%;
-      color: #a9a9a9;
-      transition: all 0.3s ease-in-out;
+			position: absolute;
+			top: 5%;
+			left: 1.5%;
+			color: #a9a9a9;
+			transition: all 0.3s ease-in-out;
 
-      @media (max-width: 450px) {
-        left: 5%;
-      }
+			@media (max-width: 450px) {
+				left: 5%;
+			}
 		}
 		textarea {
-      border-radius:5px;
+			border-radius: 5px;
 			width: 100%;
 			resize: none;
 			height: 204px;
 			padding: 0;
-      border:1px solid #cccccc
+			border: 1px solid #cccccc;
 		}
 	}
-  textarea:focus + label{
-    top: 1%;
-    left: 1%;
-    font-size: 13px;
-    color: #a9a9a9;
-  }
-  textarea:focus,input:focus { 
-      outline: none !important;
-      border-color: #719ECE;
-  }
-	button {
-    border:none;
-    display: flex;
-    background: #479ed4;
-    border-radius: 8px;
-    width: 100px;
-    height: 40px;
-    text-align: center;
-    justify-content: center;
-    align-items: center;
-    font-weight: bold;
-    text-transform: capitalize;
-    color: white;
-    transition: all .3s ease-in-out;
-    margin-top: 1.5rem;
-    font-size: 1.2rem;
+	textarea:focus + label {
+		top: 1%;
+		left: 1%;
+		font-size: 13px;
+		color: #a9a9a9;
 	}
-  button:hover {
-    transform: translateY(-3px);
-    box-shadow: 0px 1px 5px #479ed4;
-    background: #397ba5;
-    cursor: pointer;
-  }
+	textarea:focus,
+	input:focus {
+		outline: none !important;
+		border-color: #719ece;
+	}
+	button {
+		border: none;
+		display: flex;
+		background: #479ed4;
+		border-radius: 8px;
+		width: 100px;
+		height: 40px;
+		text-align: center;
+		justify-content: center;
+		align-items: center;
+		font-weight: bold;
+		text-transform: capitalize;
+		color: white;
+		transition: all 0.3s ease-in-out;
+		margin-top: 1.5rem;
+		font-size: 1.2rem;
+	}
+	button:hover {
+		transform: translateY(-3px);
+		box-shadow: 0px 1px 5px #479ed4;
+		background: #397ba5;
+		cursor: pointer;
+	}
 `;
